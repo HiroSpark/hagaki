a08ff427527f4f005672697bd95b4335b8b6f17d
HiroSpark <73699844+HiroSpark@users.noreply.github.com>
2022-05-22 15:15:44 +0900

    build: 自動タグ付けを改善
    `l3build tag` に渡すタグ名を、major/minor/patch の 3 つに絞ることで、
    ミスを防ぐ。

--- build.lua
+++ build.lua
@@ -24,26 +24,44 @@ end
 
 tagfiles = {"hagaki.cls", "doc/hagaki.tex"}
 
-function update_tag(file, content, tagname, tagdate)
-  if string.match(tagname, "^%d+%.%d+%.%d+$") ~= nil then
-    tagdate = string.gsub(tagdate, "-", "/")
-    if string.match(file, "%.tex$") then
-      return string.gsub(
-        content,
-        "\\date{.-（.-）}",
-        "\\date{v" .. tagname .. "（" .. tagdate .. "）}"
-      )
-    elseif string.match(file, "%.cls$") then
-      return string.gsub(
-        content,
-        "\\ProvidesExplClass{(.-)}{.-}{.-}",
-        "\\ProvidesExplClass{%1}{" .. tagdate .. "}{" .. tagname .. "}"
-      )
-    end
-    return content
+function update_tag(file, content, tagtype, tagdate)
+  -- タグの作成
+  local handle = assert(
+    io.popen("git describe --tags --abbrev=0"),
+    "最新のタグを取得できませんでした"
+  )
+  local current = handle:read("a") handle:close()
+  local major, minor, patch = assert(string.match(current, "v(%d-)%.(%d-)%.(%d+)"))
+  -- バージョンを更新
+  if tagtype == "major" then
+    major = math.tointeger(major + 1)
+    minor = 0
+    patch = 0
+  elseif tagtype == "minor" then
+    minor = math.tointeger(minor + 1)
+    patch = 0
+  elseif tagtype == "patch" then
+    patch = math.tointeger(patch + 1)
   else
-    error("タグの名前が誤っています。")
+    error("tag のタイプは、major/minor/patch のいずれかにしてください")
   end
+  local tagname = major .. "." .. minor .. "." .. patch
+  -- タグを更新
+  tagdate = string.gsub(tagdate, "-", "/")
+  if string.match(file, "%.tex$") then
+    return string.gsub(
+      content,
+      "\\date{.-（.-）}",
+      "\\date{v" .. tagname .. "（" .. tagdate .. "）}"
+    )
+  elseif string.match(file, "%.cls$") then
+    return string.gsub(
+      content,
+      "\\ProvidesExplClass{(.-)}{.-}{.-}",
+      "\\ProvidesExplClass{%1}{" .. tagdate .. "}{" .. tagname .. "}"
+    )
+  end
+  return content
 end
 
 function tag_hook(tagname)
